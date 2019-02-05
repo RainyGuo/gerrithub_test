b46bf2bcb3ffe7e78b17aa15aba958cd997e6af4
Rainy <305544393@qq.com>
2019-02-05 12:37:34 -0500

    change a to 3

--- testfile.java
+++ testfile.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
-        int a = 1; 
+        int a = 3; 
         int b = 2; 
         // It will return the sum of two arguments. 
         System.out.println(Integer.sum(a, b));
