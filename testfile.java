88bb8676391fcab3ce9bb8359304072052411777
Rainy <305544393@qq.com>
2019-02-05 12:37:06 -0500

    changed to 2

--- testfile.java
+++ testfile.java
@@ -2,7 +2,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         int a = 1; 
-        int b = 1; 
+        int b = 2; 
         // It will return the sum of two arguments. 
         System.out.println(Integer.sum(a, b));
     }
